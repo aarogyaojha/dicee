7cc44794b077ab305eecbf20fe9638b52ad7f8ce
aarogyaojha <arogyaojha251@gmail.com>
2025-04-12 10:15:34 +0530

    randomizing the images for dice

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -22,25 +24,31 @@ class DicePage extends StatefulWidget {
   @override
   State<DicePage> createState() => _DicePageState();
 }
+
 class _DicePageState extends State<DicePage> {
   int leftDiceNumber = 1;
   int rightDiceNumber = 1;
   @override
   Widget build(BuildContext context) {
-    int leftDiceNumber = 6;
     return Center(
       child: Row(
         children: [
           Expanded(
             child: TextButton(
-              onPressed: () {},
+              onPressed: () {
+                setState(() {
+                  leftDiceNumber = Random().nextInt(6) + 1;
+                });
+              },
               child: Image.asset("images/dice$leftDiceNumber.png"),
             ),
           ),
           Expanded(
             child: TextButton(
               onPressed: () {
-                print("right button");
+                setState(() {
+                  rightDiceNumber = Random().nextInt(6) + 1;
+                });
               },
               child: Image.asset("images/dice$rightDiceNumber.png"),
             ),
